0f811848dec690be21194ca2566836d27bf26e90
Martin Chulev <martin.chulev@abv.bg>
2019-01-16 17:46:11 +0200

    Game setup, Player and Camera section finished.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin;
+    public float xMax;
+    public float zMin;
+    public float zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") == true && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject shotClone = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+        rb.velocity = movement;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
