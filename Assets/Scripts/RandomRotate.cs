88d5bb38b539bf69020edd43804e9d0be8b9943f
Martin Chulev <martin.chulev@abv.bg>
2019-01-17 02:36:25 +0200

    Boundaries, Hazards and Enemies finished.

---
+++ Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotate : MonoBehaviour
+{
+    public float tumble;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere*tumble;
+    }
+
+    void Update()
+    {
+        
+    }
+}
